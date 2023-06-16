6ad8b245e81fc53f71473aab6f38abbcc0f40323
offbrandcat <128991168+offbrandcat@users.noreply.github.com>
2023-06-15 21:36:18 -0400

    last commit

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { typeDefs, resolvers } = require('./schemas');
 
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,9 +16,10 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-server.applyMiddleware({ app });
+// Error: You must `await server.start()` before calling `server.applyMiddleware()`??
+// server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -26,9 +27,25 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`Server now running on port ${PORT}`);
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/"));
 });
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
